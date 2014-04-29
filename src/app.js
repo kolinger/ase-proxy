3c64506048f91c059e0acf20addbbce50f6ebf68
Tomáš Kolinger <tomas@kolinger.name>
2014-04-29 12:14:24 +0200

    added debugging

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ var zmq = require('zmq');
 
 var routerAddress = process.argv[2]; // first arguments is router address
 var publisherAddress = process.argv[3]; // second is publisher address
+var debugMode = process.argv[4]; // debug mode
 
 // bind publisher on specific address
 var publisher = zmq.socket('pub');
@@ -24,6 +25,10 @@ router.bind('tcp://' + routerAddress, function(err) {
 });
 
 // resend received messages with publisher to all subscribers
+var count = 0;
 router.on('message', function(envelope, request) {
+	if (debugMode) {
+		console.log('Receiving message #' + (++count));
+	}
     publisher.send(request);
 });
